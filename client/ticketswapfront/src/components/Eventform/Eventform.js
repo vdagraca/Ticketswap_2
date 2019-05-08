a027d84465865915450aaec0fb1166398c540378
Vivian da Graca <vivian.da.graca@gmail.com>
2019-05-08 09:16:23 +0200

    form works

---
+++ client/ticketswapfront/src/components/Eventform/Eventform.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class EventForm extends Component {
+    render() {
+        return (
+            <div>
+                <form className={'Form'}>
+                    <label>Name:
+                        <input type='text' name='name' value={this.props.values['name']} onChange={this.props.onChange} />
+                    </label>
+                    <label>Description:
+                        <input type='text' name='description' value={this.props.values['description']} onChange={this.props.onChange} />
+                    </label>
+                    <label>Picture:
+                        <input type='text' name='picture' value={this.props.values['picture']} onChange={this.props.onChange} />
+                    </label>
+                    <label>Startdate:
+                        <input type='text' name='startdate' value={this.props.values['startdate']} onChange={this.props.onChange} />
+                    </label>
+                    <label>Enddate:
+                        <input type='text' name='enddate' value={this.props.values['enddate']} onChange={this.props.onChange} />
+                    </label>
+                    <button onClick={this.props.onSubmit} type='submit' value="Submit">Send</button>
+                </form>
+            </div>
+        )
+    }
+}
