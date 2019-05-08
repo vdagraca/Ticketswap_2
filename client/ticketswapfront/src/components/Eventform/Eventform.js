b9bfde70b359ab0dd071305e8bf1b5c354f4fa0e
Vivian da Graca <vivian.da.graca@gmail.com>
2019-05-08 08:44:40 +0200

    form fields

--- client/ticketswapfront/src/components/Eventform/Eventform.js
+++ client/ticketswapfront/src/components/Eventform/Eventform.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Eventform extends Component {
+export default class EventForm extends Component {
     render() {
         return (
             <div>
--- client/ticketswapfront/src/components/Eventform/EventformContainer.js
+++ client/ticketswapfront/src/components/Eventform/EventformContainer.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
+import EventForm from './EventForm'
+import { connect } from 'react-redux';
+// import { createEvent } from '../../actions/events'
 
-export default class EventformContainer extends Component {
+export class EventFormContainer extends Component {
+    state = {
+        name: '',
+        description: '',
+        picture: '',
+        startdate: '',
+        enddate: '',
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // onSubmit = (event) => {
+    //     event.preventDefault()
+
+    //     this.setState({
+    //         title: '',
+    //         description: '',
+    //         picture: '',
+    //         price: '',
+    //         email: '',
+    //         phonenumber: ''
+
+    //     })
+    //     this.props.createEvent(this.state)
+    // }
+
     render() {
-        return (
-            <div>
 
+        return (
+
+            <div>
+                <EventForm
+                    onSubmit={this.onSubmit}
+                    onChange={this.onChange}
+                    values={this.state} />
             </div>
         )
     }
 }
+
+export default connect(null)(EventFormContainer)
