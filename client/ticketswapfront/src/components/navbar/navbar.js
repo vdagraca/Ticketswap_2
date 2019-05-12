c72317d9eb86c483e162f51dedc9022f85243599
Vivian da Graca <vivian.da.graca@gmail.com>
2019-05-12 19:15:11 +0200

    css

--- client/ticketswapfront/src/components/navbar/navbar.js
+++ client/ticketswapfront/src/components/navbar/navbar.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
+import './navbar.css';
 
 
 class navbar extends Component {
     render() {
         return (
-
             <div>
-                Ticketswap
-                <Link to={`/login`} className="Loginbutton">Login</Link>
-                <Link to={`/logout`} className="Logoutbutton">Logout</Link>
-                <Link to={`/signup`} className="Signupbutton">Sign up</Link>
-                <div />
+                <div className='navbar'>
+                    <div className='logcontainer'>
+                        <Link style={{ textDecoration: 'none', color: 'white' }} to={`/login`} className="Loginbutton">Login</Link>
+                        <Link style={{ textDecoration: 'none', color: 'white' }} to={`/logout`} className="Logoutbutton">Logout</Link>
+                    </div>
+                    <div classname='title'>Ticketswap
+                </div>
+                    <div className='signupcontainer'>
+                        <Link className='signupcontainer' style={{ textDecoration: 'none', color: 'white' }} to={`/signup`} className="Signupbutton">Sign up</Link>
+                    </div>
 
-                {this.props.user &&
-                    <div>
+                </div>
+                {
+                    this.props.user &&
+                    <div className='username'>
                         {this.props.user.firstName}{this.props.user.lastName}
-                    </div>}
-
-            </div>
+                    </div>
+                }
+            </div >
 
 
         )
