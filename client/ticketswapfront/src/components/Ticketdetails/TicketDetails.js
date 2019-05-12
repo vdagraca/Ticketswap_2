f599be2220b1ceb464e26a223fbab6a8c910a625
Vivian da Graca <vivian.da.graca@gmail.com>
2019-05-12 22:03:46 +0200

    eventname in ticket

--- client/ticketswapfront/src/components/Ticketdetails/TicketDetails.js
+++ client/ticketswapfront/src/components/Ticketdetails/TicketDetails.js
@@ -5,7 +5,7 @@ import './TicketDetails.css';
 export default class TicketDetails extends Component {
 
     render() {
-        const { ticket, fraude, currentUser } = this.props
+        const { ticket, fraude, currentUser, event } = this.props
         let className = () => {
             if (fraude === 5) {
                 return 'green'
@@ -21,6 +21,7 @@ export default class TicketDetails extends Component {
             <div>
                 <h1>Ticket from {ticket.userName}</h1>
                 <h2 className={className()}>Risk:{fraude}%</h2>
+                <h2>{event.name}</h2>
                 <li>Description:{ticket.description}</li>
                 <br />
                 <img className="image" style={{ width: "300px" }} src={ticket.picture} alt={ticket.name} />
