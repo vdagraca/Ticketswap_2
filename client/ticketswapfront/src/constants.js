e5c14386f13ce7a476c4bd6ef49dedcd606c5aa1
Vivian da Graca <vivian.da.graca@gmail.com>
2019-06-13 14:23:20 +0200

    heroku

--- client/ticketswapfront/src/constants.js
+++ client/ticketswapfront/src/constants.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'http://localhost:4001'
+// export const baseUrl = 'http://localhost:4001'
+export const baseUrl ='https://grozeries3.herokuapp.com'
 
 export const localStorageJwtKey = 'currentUserJwt'
